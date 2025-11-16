66277c77077536e556f7d74225908d8b58c4e199
Phani Kumar <phanikumar0610@gmail.com>
2025-11-16 14:09:56 -0500

    Implement array operations: accessing, iterating, and inserting elements

---
+++ src/amazonDs/module1/OperationsInArrays.java
@@ -0,0 +1,46 @@
+package src.amazonDs.module1;
+
+public class OperationsInArrays {
+
+    public static void main(String[] args){
+
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        //accessing by index
+        System.out.println("First day: " + daysOfWeek[0]); // Monday
+        //find the length
+        System.out.println("Number of days in a week: " + daysOfWeek.length);
+
+        //iterating through array
+        for(int index=0; index< daysOfWeek.length; index++){
+            System.out.println("Day " + (index+1) + ": " + daysOfWeek[index]);
+        }
+        
+        //inserting an element
+        String[] daysOfWeek1 = {"Monday", "Tuesday", "Wednesday", "Thur", "Friday", "Saturday", "Sunday"};
+        daysOfWeek1[3] = "Thursday"; //correcting the value at index 3
+        for(int index=0; index< daysOfWeek1.length; index++){
+            System.out.println("Day " + (index+1) + ": " + daysOfWeek1[index]);
+        }
+
+        //inserting an element at the end of the array
+        String[] monthOfYear = new String[]{"Jan", "Feb", "Mar", "Apr", "May"};
+
+        String newElement = "Jun";
+        String[] tempArray = new String[6];
+        for(int index=0; index< tempArray.length; index++){
+            if( index == (tempArray.length -1)){    
+                tempArray[index] = newElement;
+            } else {
+                tempArray[index] = monthOfYear[index];
+            }
+        }
+
+        for(int index=0; index< tempArray.length; index++){
+            System.out.println("Month " + (index+1) + ": " + tempArray[index]);
+        }
+
+    }
+
+    
+}
